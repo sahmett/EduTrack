0fe965dfc9dc3b9b81350bd2e34407840c95343b
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2024-01-16 05:24:30 +0300

    [added] user configuration added and db mig update

---
+++ EduTrack.Persistence/Migrations/20240116021229_username.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduTrack.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class username : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedByUserId",
+                table: "AspNetUsers",
+                type: "character varying(75)",
+                maxLength: 75,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "character varying(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "character varying(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByUserId",
+                table: "AspNetUsers",
+                type: "character varying(75)",
+                maxLength: 75,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedByUserId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(75)",
+                oldMaxLength: 75,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByUserId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(75)",
+                oldMaxLength: 75,
+                oldNullable: true);
+        }
+    }
+}
